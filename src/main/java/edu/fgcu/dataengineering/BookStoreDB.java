035c18c8c0c5c321962f14183d5a627bb6935001
oscargarciavazquez <ovgarcia5138@eagle.fgcu.edu>
2020-02-12 19:50:10 -0500

    loading

--- src/main/java/edu/fgcu/dataengineering/BookStoreDB.java
+++ src/main/java/edu/fgcu/dataengineering/BookStoreDB.java
@@ -9,4 +9,81 @@ import java.sql.Statement;
 
 public class BookStoreDB {
 
+    private static Database db;
+
+    /**
+     * This method returns a data base when called if it is not already created.
+     *
+     * @return A data base.
+     */
+    public static Database getDb() {
+        if (db == null) {
+            db = new Database();
+        }
+        return db;
+    }
+
+    /**
+     * This method method will establish a connection the the created H2 data base.
+     */
+    public static class Database {
+        private Connection conn;
+        private Statement stmt;
+
+        public Database() {
+            //final String JDBC_DRIVER = "org.sqlite.Driver";
+            final String DB_URL = "jdbc:sqlite:/Users/oscargarciavazquez/IdeaProjects/CsvToDatabase/src/Data/BookStore.db";
+
+            final String USER = "";
+            final String PASS = "";
+
+            try {
+                conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                stmt = conn.createStatement();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        /**
+         * This method will return a Connection or create one if it does not exist yet.
+         *
+         * @return A connection.
+         */
+        public Connection getConn() {
+            try {
+                if (conn.isClosed()) {
+                    //final String JDBC_DRIVER = "org.h2.Driver";
+                    final String DB_URL = "jdbc:sqlite:/Users/oscargarciavazquez/IdeaProjects/CsvToDatabase/src/Data/BookStore.db";
+
+                    final String USER = "";
+                    final String PASS = "";
+
+                    conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return conn;
+        }
+
+        /**
+         * This method will return a Statement or create one if it does not exist yet.
+         *
+         * @return A statement.
+         * @param sqlQuery
+         */
+        public Statement getStmt(String sqlQuery) {
+            try {
+                if (stmt.isClosed()) {
+                    stmt = getConn().createStatement();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return stmt;
+        }
+
+    }
+
 }
--- src/main/java/edu/fgcu/dataengineering/Main.java
+++ src/main/java/edu/fgcu/dataengineering/Main.java
@@ -2,16 +2,42 @@ package edu.fgcu.dataengineering;
 
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
+import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
+
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, CsvValidationException {
-	      // Literally just calls our parser right now (....and is called for tests)
+    public static void main(String[] args) throws IOException, CsvValidationException, SQLException {
+        // Literally just calls our parser right now (....and is called for tests)
         CsvParser csvP = new CsvParser("src/Data/bookstore_report2.csv");
         csvP.printCsv();
+        Reader read = Files.newBufferedReader(Paths.get("src/Data/bookstore_report2.csv"));
+        CSVReader reader = new CSVReader(read);
+
+        String[] bookInfo;
+
+        while ((bookInfo = reader.readNext()) != null) {
+            String sqlInsert = "INSERT INTO book(isbn, book_title, author_name, publisher_name) " +
+                    "VALUES(?,?,?,?)";
+            try {
+                PreparedStatement pstatement = BookStoreDB.getDb().getConn().prepareStatement(sqlInsert);
+                pstatement.setString(1, bookInfo[0]);
+                pstatement.setString(2,bookInfo[1]);
+                pstatement.setString(3,bookInfo[2]);
+                pstatement.setString(4,bookInfo[3]);
+                pstatement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
         // Load the json
         /*
@@ -24,8 +50,40 @@ public class Main {
         JsonReader jread = new JsonReader(new FileReader("src/Data/authors.json"));
         AuthorParser[] authors = gson.fromJson(jread, AuthorParser[].class);
 
+        
+        try {
+            String name = "hello";
+            String email = "bye";
+            String url  = "please";
+
+            String sqlQuery = "INSERT INTO author(author_name, author_email, author_url) VALUES (?, ?, ?)";
+            PreparedStatement pstatement = BookStoreDB.getDb().getConn().prepareStatement(sqlQuery);
+
+
+            for ( var element : authors) {
+                pstatement.setString(1,element.getName());
+                pstatement.setString(2,element.getEmail());
+                pstatement.setString(3,element.getUrl());
+                pstatement.executeUpdate();
+            }
+
+            System.out.println("hello");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
         for (var element : authors) {
             System.out.println(element.getName());
+            System.out.println(element.getEmail());
+            System.out.println(element.getUrl());
         }
+
+
+
+
+
     }
+
+
 }
